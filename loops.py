b845f0400c17f8a261be48b420a77c7f6aa1bf72
P.Bharathi <bharathi.be115@gmail.com>
2026-03-26 19:13:15 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,90 @@
+#loop
+'''
+b = [2,3,4,6]#list
+print(7 in b)
+print(4 in b)
+print(4 not in b)
+print(7 not in b)
+'''
+#For loop
+"""
+1.for - integer => inc/dec
+2.for - string
+syntax of for loop ->interger method ->inc
+        1     2                          4
+for variable in range(start,stop[n-1],step[inc+]):
+            3
+    #looping statement
+"""
+#inc
+#1
+"""
+for i in range(1,6,1):#12345
+    print(i)
+#2
+for i in range(0,51,5):#0 to 5
+    print(i)
+#3
+for i in range(40,51):
+    print(i)
+#4
+for i in range(51):
+    print(i)
+"""
+'''
+syntax of for loop ->interger method ->inc
+        1     2                          4
+for variable in range(start,stop[n-1],step[inc+]):
+            3
+    #looping statement
+'''
+# 1 2-F               4
+'''
+for i in range (10,5,-2):#109876#6-2=4
+        #3
+    print(i)
+'''
+#python demo Tasks
+#1.factorial => 5,6
+'''
+5*4*3*2*1 =>120
+6*5*4*3*2*1 =>720
+'''
+'''
+n = int(input("Enter Value:"))#5
+fact=1
+for i in range(1,n+1):#12345
+    fact*=i
+print("Factorial Value:",fact)
+'''
+#sum of natural value upto 10
+n = int(input("Enter Value:"))#5
+s=0
+for i in range(1, n+1):#12345
+    s+=i
+print("sum value:",s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
